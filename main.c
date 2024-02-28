a5ffa36c196f39503f18120e463083dfea4961ce
ilyaBurPv232 <pv.232.ilya.burtcev@gmail.com>
2024-02-28 10:41:16 +0300

    The second task has been solved
    +getMax
    +sortRowsByMinElement
    *Solve + tests

--- main.c
+++ main.c
@@ -16,19 +16,61 @@ void test_task1() {
                                      3, 3);
 
     matrix m_test = createMatrixFromArray((int[]) {3, 3, 3,
-                                              2, 2, 2,
-                                              1, 1, 1},
-                                     3, 3);
+                                                   2, 2, 2,
+                                                   1, 1, 1},
+                                          3, 3);
 
     task1(&m);
 
-    assert(areTwoMatricesEqual(&m,&m_test));
+    assert(areTwoMatricesEqual(&m, &m_test));
     freeMemMatrix(&m);
     freeMemMatrix(&m_test);
 }
 
+int getMax(int *a, int n) {
+    int max = a[0];
+
+    for (int i = 1; i < n; ++i) {
+        max = max > a[i] ? max : a[i];
+    }
+
+    return max;
+}
+
+void sortRowsByMinElement(matrix *m){
+    insertionSortRowsMatrixByRowCriteria(m,getMax);
+}
+
+void task2(matrix *m){
+    sortRowsByMinElement(m);
+}
+
+void test_task2(){
+    matrix m = createMatrixFromArray((int[]) {12, 9, 9,
+                                              11, 9, 9,
+                                              10, 9, 9},
+                                     3, 3);
+
+    matrix m_test = createMatrixFromArray((int[]) {10, 9, 9,
+                                                   11, 9, 9,
+                                                   12, 9, 9},
+                                          3, 3);
+
+    task2(&m);
+
+    assert(areTwoMatricesEqual(&m, &m_test));
+    freeMemMatrix(&m);
+    freeMemMatrix(&m_test);
+}
+
+void test(){
+    //test_task1();
+    test_task2();
+}
+
 int main() {
-    test_task1();
+
+    test();
 
     return 0;
 }
